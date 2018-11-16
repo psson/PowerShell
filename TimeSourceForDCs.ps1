65f6b4652c21bcebb5457c5cd775dc122e83d0f3
Andreas Pettersson <psson@IT4895>
2018-11-16 10:29:57 +0100

    Create TimeSourceForDCs.ps1
    Created simple script to check time source for all domain controllers in a domain

---
+++ TimeSourceForDCs.ps1
@@ -0,0 +1,10 @@
+﻿# The script checks the time source for all DCs in the current domain
+
+$DomainControllers = Get-ADDomainController -Filter * | Select-Object -ExpandProperty hostname
+$Separator = "-----"
+Write-Host $Separator
+foreach ($DomainController in $DomainControllers) {
+    Write-Host $DomainController
+    w32tm /query /source /computer:$DomainController
+    Write-Host $Separator
+}
